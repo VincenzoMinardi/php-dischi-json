72a11a250b884690e879d902768f6fa70f53568d
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-09 13:54:13 +0200

    add: script

---
+++ script.js
@@ -0,0 +1,37 @@
+const Artist = [
+  {
+    name_song: "Wayne Barnett",
+    artist: "Founder & CEO",
+    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+  },
+
+  {
+    name_song: "Wayne Barnett",
+    artist: "Founder & CEO",
+    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+  },
+
+  {
+    name_song: "Wayne Barnett",
+    artist: "Founder & CEO",
+    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+  },
+
+  {
+    name_song: "Wayne Barnett",
+    artist: "Founder & CEO",
+    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+  },
+
+  {
+    name_song: "Wayne Barnett",
+    artist: "Founder & CEO",
+    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+  },
+
+  {
+    name_song: "Wayne Barnett",
+    artist: "Founder & CEO",
+    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+  },
+];
