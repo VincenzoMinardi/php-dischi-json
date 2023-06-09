d504b0423e9a536f3fd674bd3088f0b4eb311e80
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-09 15:06:05 +0200

    add: image with style is vue

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-const Artist = [
-  {
-    img: `<img src="img/NewJersey.jpg" alt="">`,
-    name_song: "New Jersey",
-    artist: "Bon jovi",
-    data: "1988",
-  },
+const { createApp } = Vue;
 
-  {
-    img: `<img src="img/LiveatWembley.jpg" alt="">`,
-    name_song: "Live at Wembley 86",
-    artist: "Queen",
-    data: "1992",
-  },
+createApp({
+  data() {
+    return {
+      ArrSong: [
+        {
+          img: "img/NewJersey.jpg",
+          name_song: "New Jersey",
+          artist: "Bon jovi",
+          data: "1988",
+        },
 
-  {
-    img: `<img src="img/Summoner.jpg" alt="">`,
-    name_song: "Ten's Summoner's Tales",
-    artist: "Sting",
-    data: "1993",
-  },
+        {
+          img: "img/LiveatWembley.jpg",
+          name_song: "Live at Wembley 86",
+          artist: "Queen",
+          data: "1992",
+        },
 
-  {
-    img: `<img src="img/Stevegaddband.jpg" alt="">`,
-    name_song: "Steve Gadd band",
-    artist: "Steve Gadd Band",
-    data: "2018",
-  },
+        {
+          img: "img/Summoner.jpg",
+          name_song: "Ten's Summoner's Tales",
+          artist: "Sting",
+          data: "1993",
+        },
 
-  {
-    img: `<img src="img/bravenewworld.jpg" alt="">`,
-    name_song: "Brave new world",
-    artist: "Iron Maiden",
-    data: "2000",
-  },
+        {
+          img: "img/Stevegaddband.jpg",
+          name_song: "Steve Gadd band",
+          artist: "Steve Gadd Band",
+          data: "2018",
+        },
 
-  {
-    img: `<img src="img/Onemorecar.jpg" alt="">`,
-    name_song: "One more car, one more rider",
-    artist: "Eric Clapton",
-    data: "2002",
+        {
+          img: "img/bravenewworld.jpg",
+          name_song: "Brave new world",
+          artist: "Iron Maiden",
+          data: "2000",
+        },
+
+        {
+          img: "img/Onemorecar.jpg",
+          name_song: "One more car, one more rider",
+          artist: "Eric Clapton",
+          data: "2002",
+        },
+      ],
+    };
   },
-];
+}).mount("#app");
