277f8d873cd40c74756f6a40a2bd0851f1d1de90
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-09 16:22:22 +0200

    Add: collegate array php in json

--- script.js
+++ script.js
@@ -3,7 +3,44 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      ArrSong: [],
+      ArrSong: [
+        // {
+        //   img: "img/NewJersey.jpg",
+        //   name_song: "New Jersey",
+        //   artist: "Bon jovi",
+        //   data: "1988",
+        // },
+        // {
+        //   img: "img/LiveatWembley.jpg",
+        //   name_song: "Live at Wembley 86",
+        //   artist: "Queen",
+        //   data: "1992",
+        // },
+        // {
+        //   img: "img/Summoner.jpg",
+        //   name_song: "Ten's Summoner's Tales",
+        //   artist: "Sting",
+        //   data: "1993",
+        // },
+        // {
+        //   img: "img/Stevegaddband.jpg",
+        //   name_song: "Steve Gadd band",
+        //   artist: "Steve Gadd Band",
+        //   data: "2018",
+        // },
+        // {
+        //   img: "img/bravenewworld.jpg",
+        //   name_song: "Brave new world",
+        //   artist: "Iron Maiden",
+        //   data: "2000",
+        // },
+        // {
+        //   img: "img/Onemorecar.jpg",
+        //   name_song: "One more car, one more rider",
+        //   artist: "Eric Clapton",
+        //   data: "2002",
+        // },
+      ],
     };
   },
 
@@ -11,7 +48,11 @@ createApp({
     requestAlbums() {
       axios
         .get("http://localhost:8888/php-dischi-json/server.php")
-        .then(response => (this.arr_Song = response.data));
+        .then(response => (this.ArrSong = response.data));
     },
+  },
+
+  created() {
+    this.requestAlbums();
   },
 }).mount("#app");
--- server.php
+++ server.php
@@ -1,39 +1,45 @@
 <?php
-$Arr_Song = [
-    [
-        'img' => 'img/NewJersey.jpg',
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
-        'year' => '1988'
-    ],
-    [
-        'img' => 'img/LiveatWembley.jpg',
-        'title' => 'Live at Wembley 86',
-        'author' => 'Queen',
-        'year' => '1992'
-    ],
-    [
-        'img' => 'img/Summoner.jpg',
-        'title' => "Ten's Summoner's Tales",
-        'author' => 'Sting',
-        'year' => '1993'
-    ],
-    [
-        'img' => 'img/Stevegaddband.jpg',
-        'title' => 'Steve Gadd band',
-        'author' => 'Steve Gadd Band',
-        'year' => '2018'
-    ],
-    [
-        'img' => 'img/bravenewworld.jpg',
-        'title' => 'Brave new world',
-        'author' => 'Iron Maiden',
-        'year' => '2000'
-    ],
-    [
-        'img' => 'img/Onemorecar.jpg',
-        'title' => 'One more car one more rider',
-        'author' => 'Eric Clapton',
-        'year' => '2002'
-    ]
-];
+$ArrSong = file_get_contents('cards-albums.json');
+// $ArrSong = [
+//     [
+//         'img' => 'img/NewJersey.jpg',
+//         'title' => 'New Jersey',
+//         'author' => 'Bon Jovi',
+//         'year' => '1988'
+//     ],
+//     [
+//         'img' => 'img/LiveatWembley.jpg',
+//         'title' => 'Live at Wembley 86',
+//         'author' => 'Queen',
+//         'year' => '1992'
+//     ],
+//     [
+//         'img' => 'img/Summoner.jpg',
+//         'title' => "Ten's Summoner's Tales",
+//         'author' => 'Sting',
+//         'year' => '1993'
+//     ],
+//     [
+//         'img' => 'img/Stevegaddband.jpg',
+//         'title' => 'Steve Gadd band',
+//         'author' => 'Steve Gadd Band',
+//         'year' => '2018'
+//     ],
+//     [
+//         'img' => 'img/bravenewworld.jpg',
+//         'title' => 'Brave new world',
+//         'author' => 'Iron Maiden',
+//         'year' => '2000'
+//     ],
+//     [
+//         'img' => 'img/Onemorecar.jpg',
+//         'title' => 'One more car one more rider',
+//         'author' => 'Eric Clapton',
+//         'year' => '2002'
+//     ]
+// ];
+
+$json_string = json_encode($ArrSong);
+// file_put_contents('cards-albums.json', $json_string);
+
+echo ($ArrSong);
