4dec72a66bec10e52427b6e390ad9e2a3a35845b
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-09 14:04:42 +0200

    add: array with artist

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 const Artist = [
   {
-    name_song: "Wayne Barnett",
-    artist: "Founder & CEO",
-    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+    img: `<img src="img/NewJersey.jpg" alt="">`,
+    name_song: "New Jersey",
+    artist: "Bon jovi",
+    data: "1988",
   },
 
   {
-    name_song: "Wayne Barnett",
-    artist: "Founder & CEO",
-    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+    img: `<img src="img/LiveatWembley.jpg" alt="">`,
+    name_song: "Live at Wembley 86",
+    artist: "Queen",
+    data: "1992",
   },
 
   {
-    name_song: "Wayne Barnett",
-    artist: "Founder & CEO",
-    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+    img: `<img src="img/Summoner.jpg" alt="">`,
+    name_song: "Ten's Summoner's Tales",
+    artist: "Sting",
+    data: "1993",
   },
 
   {
-    name_song: "Wayne Barnett",
-    artist: "Founder & CEO",
-    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+    img: `<img src="img/Stevegaddband.jpg" alt="">`,
+    name_song: "Steve Gadd band",
+    artist: "Steve Gadd Band",
+    data: "2018",
   },
 
   {
-    name_song: "Wayne Barnett",
-    artist: "Founder & CEO",
-    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+    img: `<img src="img/bravenewworld.jpg" alt="">`,
+    name_song: "Brave new world",
+    artist: "Iron Maiden",
+    data: "2000",
   },
 
   {
-    name_song: "Wayne Barnett",
-    artist: "Founder & CEO",
-    img: `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`,
+    img: `<img src="img/Onemorecar.jpg" alt="">`,
+    name_song: "One more car, one more rider",
+    artist: "Eric Clapton",
+    data: "2002",
   },
 ];
